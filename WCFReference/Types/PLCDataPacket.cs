f3c5d51058cfa563304ae8e377340d7b08c20f8f
Mark Cafazzo <mark.cafazzo@capacitive.ca>
2014-03-19 14:27:00 -0400

    PLCDataPacket helper functions

--- WCFReference/Types/PLCDataPacket.cs
+++ WCFReference/Types/PLCDataPacket.cs
@@ -11,10 +11,92 @@ namespace WCFReferenceService
     [Serializable]
     public class PLCDataPacketConfiguration
     {
+        List<DataByte> _byteList = new List<DataByte>();
+
         [DataMember]
-        public List<DataByte> Bytes { get; set; }
+        public List<DataByte> Bytes 
+        { 
+            get { return _byteList; } 
+            set { _byteList = value; }
+        }      
+
         [DataMember]
         public bool IsDecoder { get; set; }
+
+        //public bool AddHeader()
+        //{
+        //    try
+        //    {
+        //        DataByte header = new DataByte();
+        //        header.IsHeaderOrFooter = true;
+        //        header.ByteIndex = 0;
+        //        _byteList.Add(header);
+        //    }
+        //    catch
+        //    {
+        //        return false; 
+        //    }
+        //    return true;
+        //}
+
+        //public bool AddByteWithBitFlags()
+        //{
+        //    try
+        //    {
+        //        int lastIndex = Bytes.FindLastIndex(FindDataByte);
+
+        //        DataByte byteWithBits = new DataByte();
+        //        byteWithBits.ContainsBits = true;
+        //        byteWithBits.ByteIndex = lastIndex + 1;
+
+        //        _byteList.Add(byteWithBits);
+        //    }
+        //    catch
+        //    {
+                
+        //        return false;
+        //    }
+        //    return true;
+        //}
+
+        //public bool AddFooter(int byteIndex)
+        //{
+        //    int lastIndex = Bytes.FindLastIndex(FindDataByte);
+        //    if (lastIndex > -1)
+        //    {
+        //        DataByte footer = new DataByte();
+        //        footer.IsHeaderOrFooter = true;
+        //        footer.ByteIndex = byteIndex;
+
+        //        //add the remaining Bytes required as placeholders (desired ByteIndex - current ByteIndex = remaining Byte "slots" required):
+        //        int placeholderCount = byteIndex - lastIndex;
+        //        for (int i = lastIndex + 1; i <= placeholderCount; i++)
+        //        {
+        //            DataByte placeHolder = new DataByte();
+        //            placeHolder.ByteIndex = i;
+        //            _byteList.Add(placeHolder);
+        //        }
+
+        //        _byteList.Add(footer);
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //    return true;
+        //}
+
+        //private static bool FindDataByte(DataByte dByte)
+        //{
+        //    if (dByte.ByteIndex > 0)
+        //    {
+        //        return true;
+        //    }
+        //    else 
+        //    {
+        //        return false;
+        //    }
+        //}
     }
 
     [DataContract]
--- WCFReferenceServiceClient/Program.cs
+++ WCFReferenceServiceClient/Program.cs
@@ -1,26 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WCFReferenceServiceClient.WCFRef;
 
 namespace WCFReferenceServiceClient
 {
     class Program
     {
+        static PLCDataPacketConfiguration _newPacketConfig = new PLCDataPacketConfiguration();
+
         static void Main(string[] args)
         {
             var proxy = new PLCDataServiceContractClient("TCPEndpoint");
 
-            PLCDataPacketConfiguration newPacketConfig = new PLCDataPacketConfiguration();
-
             //1a. specify if this configuration is a decoder or encoder:
-            newPacketConfig.IsDecoder = true;
+            _newPacketConfig.IsDecoder = true;
 
             List<DataByte> byteList = new List<DataByte>();
 
-            //1b. start adding to the list of Bytes in this packet (start with header):
+            //1b. start adding to the list of Bytes in this packet (start with header - entered by user in case of getting working with packet with no header/footer):
             DataByte dbyteHeader = new DataByte();
             dbyteHeader.IsHeaderOrFooter = true;
             dbyteHeader.ByteIndex = 0;
@@ -38,7 +36,7 @@ namespace WCFReferenceServiceClient
             //not able to do this in a foreach related to UI control (array length is indeterminate):
             DataBit[] dBitArray = { 
                                       new DataBit { AGVTags = new int[135], BitIndex = 1, Description = "HVAC Pickside"},
-                                      new DataBit { AGVTags = new int[205], BitIndex = 2, Description = "Wires Lineside" },
+                                      new DataBit { AGVTags = new int[205], BitIndex = 2, Description = "Wires Lineside" },         
                                       new DataBit { AGVTags = new int[220], BitIndex = 3, Description = "Wires Pickside" },
                                       new DataBit { AGVTags = new int[220], BitIndex = 4, Description = "Steering Lineside" },
                                       new DataBit { AGVTags = new int[305], BitIndex = 5, Description = "Steering Pickside" },
@@ -50,33 +48,125 @@ namespace WCFReferenceServiceClient
             dbyte1.Bits = bitList.ToArray();
             byteList.Add(dbyte1);
             
-            //5. add a footer (remaining Bytes not specified will be backfilled or ignored when decoding/encoding)
+            //5. add a footer (remaining Bytes not specified will be backfilled or ignored when decoding/encoding - will need to be entered by user)
             DataByte dbyteFooter = new DataByte();
             dbyteFooter.IsHeaderOrFooter = true;
             dbyteFooter.ByteIndex = 11;
             byteList.Add(dbyteFooter);
 
-            newPacketConfig.Bytes = byteList.ToArray(); //new DataByte[] {dbyteHeader, dbyte1, dbyteFooter}; //again, this is not iterator friendly
+            _newPacketConfig.Bytes = byteList.ToArray(); //new DataByte[] {dbyteHeader, dbyte1, dbyteFooter}; //again, this is not iterator friendly
 
             Console.WriteLine("Enter the name of the configuration you would like to store/retrieve:");
             string configName = Console.ReadLine();
-            Console.WriteLine(@"WCF proxy method StorePLCDataPacketConfiguration() returned: {0} (true = successful storage of PLCDataPacket configuration)",
-                proxy.StorePLCDataPacketConfiguration(newPacketConfig, configName));
+            Console.WriteLine(@"WCF method StorePLCDataPacketConfiguration() returned: {0} (true = successful storage of PLCDataPacket configuration)",
+                proxy.StorePLCDataPacketConfiguration(_newPacketConfig, configName));
 
             Console.WriteLine("-------------------------------------------------------------");
-            PLCDataPacketConfiguration configFromFileIO = proxy.GetPLCDataPacketConfiguration(configName);
-            Console.WriteLine(@"WCF proxy method GetPLCDataPacketConfiguration() for returned object: {0} for PLCDataPacket configuration named: {1} contains:",
-                configFromFileIO, configName);
 
-            foreach (DataByte b in configFromFileIO.Bytes)
+            PLCDataPacketConfiguration configFromBinary = proxy.GetPLCDataPacketConfiguration(configName);
+            Console.WriteLine(@"WCF method GetPLCDataPacketConfiguration() returned PLCDataPacket configuration named: {1} which contains:",
+                configFromBinary, configName);
+
+            foreach (DataByte b in configFromBinary.Bytes)
             {
                 Console.WriteLine("DataByte index:  {0} | IsHeaderOrFooter: {1} | ContainsBitFlags: {2}", b.ByteIndex, b.IsHeaderOrFooter, b.ContainsBits);
+                if (b.ContainsBits)
+                {
+                    foreach(DataBit db in b.Bits)
+                    {
+                        Console.WriteLine("DataBit index: {0} | description: {1}", db.BitIndex, db.Description);
+                    }
+                }
             }
 
             proxy.Close();
 
             Console.WriteLine("Press <ENTER> to exit...");
             Console.Read();
+        }
+
+        public bool AddHeader(ref PLCDataPacketConfiguration config)
+        {
+            try
+            {
+                DataByte header = new DataByte();
+                header.IsHeaderOrFooter = true;
+                header.ByteIndex = 0;
+                config.Bytes.SetValue(header, 0);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool AddByteWithBitFlags(PLCDataPacketConfiguration config, List<DataBit> dataBits)
+        {
+            try
+            {
+                List<DataByte> byteList = new List<DataByte>();
+                byteList.AddRange(config.Bytes);
+                int lastIndex = byteList.FindLastIndex(FindDataByte);
+
+                DataByte byteWithBits = new DataByte();
+                byteWithBits.ContainsBits = true;
+                byteWithBits.ByteIndex = lastIndex + 1;
+                
+                foreach (DataBit dBit in dataBits)
+                {
+                    byteWithBits.Bits.SetValue(dBit, dBit.BitIndex);                                        
+                }
+
+                config.Bytes.SetValue(byteWithBits, lastIndex + 1);
+            }
+            catch
+            {
+
+                return false;
+            }
+            return true;
+        }
+
+        public bool AddFooter(int byteIndex, ref PLCDataPacketConfiguration config)
+        {
+            List<DataByte> byteList = new List<DataByte>();
+            byteList.AddRange(config.Bytes);
+            int lastIndex = byteList.FindLastIndex(FindDataByte);
+            if (lastIndex > -1)
+            {
+                DataByte footer = new DataByte();
+                footer.IsHeaderOrFooter = true;
+                footer.ByteIndex = byteIndex;
+
+                //add the remaining Bytes required as placeholders (desired ByteIndex - current ByteIndex = remaining Byte "slots" required):
+                int placeholderCount = byteIndex - lastIndex;
+                for (int i = lastIndex + 1; i <= placeholderCount; i++)
+                {
+                    DataByte placeHolder = new DataByte();
+                    placeHolder.ByteIndex = i;
+                    config.Bytes.SetValue(placeHolder, i);
+                }
+
+                config.Bytes.SetValue(footer, byteIndex);
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static bool FindDataByte(DataByte dByte)
+        {
+            if (dByte.ByteIndex > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
